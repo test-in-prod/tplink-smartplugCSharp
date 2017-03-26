41bae574058d71de2d0e9076c36a56e5a0aeb860
crypton <crypton@crypton-technologies.net>
2017-03-26 16:24:40 -0700

    Easier device enrollment CLI utility

---
+++ Crypton.TPLinkPlug.DeviceEnroll/EnrollProfile.cs
@@ -0,0 +1,191 @@
+﻿using Crypton.TPLinkPlug;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypton.TPLinkPlug.DeviceEnroll
+{
+
+    /// <summary>
+    /// Defines an enrollment profile file containing several devices
+    /// </summary>
+    public class EnrollProfile
+    {
+
+        /// <summary>
+        /// Defines a profile entry for each device
+        /// </summary>
+        public class DeviceSettings
+        {
+            /// <summary>
+            /// Matches device by its MAC address
+            /// </summary>
+            public string mac
+            {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// Defines device IP for configuration (ignored for actual devices as they are DHCP)
+            /// </summary>
+            public string ip
+            {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// Defines device SSID to connect to
+            /// </summary>
+            public string ssid
+            {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// Defines AP station password
+            /// </summary>
+            public string password
+            {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// Defines AP encryption type
+            /// </summary>
+            public WLanKeyType? encryptionType
+            {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// Defines device alias (e.g. name of device for identification)
+            /// </summary>
+            public string alias
+            {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// Sets device time to local computer's time
+            /// </summary>
+            public bool setTime
+            {
+                get;
+                set;
+            }
+        }
+
+        /// <summary>
+        /// Specifies defaults to use for all devices
+        /// </summary>
+        public DeviceSettings defaults
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Specifies list of devices to enroll
+        /// </summary>
+        public List<DeviceSettings> devices
+        {
+            get;
+            set;
+        }
+
+        public EnrollProfile()
+        {
+            defaults = new DeviceSettings();
+            devices = new List<DeviceSettings>();
+        }
+
+        public void EnrollDevices()
+        {
+            if (devices.Count == 0)
+            {
+                Console.WriteLine("Single-device enrollment");
+                devices.Add(new DeviceSettings
+                {
+                    alias = defaults.alias,
+                    encryptionType = defaults.encryptionType,
+                    ip = defaults.ip,
+                    mac = defaults.mac,
+                    password = defaults.password,
+                    setTime = defaults.setTime,
+                    ssid = defaults.ssid
+                });
+            }
+            else
+            {
+                Console.WriteLine("Multi-device enrollment");
+            }
+
+            foreach (var device in devices)
+            {
+                EnrollDevice(device);
+            }
+
+        }
+
+        private bool EnrollDevice(DeviceSettings device)
+        {
+            string alias = device.alias ?? defaults.alias;
+            string mac = device.mac ?? defaults.mac;
+            string ip = device.ip ?? defaults.ip;
+            string ssid = device.ssid ?? defaults.ssid;
+            string password = device.password ?? defaults.password;
+            var enctype = device.encryptionType ?? defaults.encryptionType ?? WLanKeyType.WPA;
+
+            var iface = new PlugInterface(ip);
+            try
+            {
+                var sysconfig = new PlugSystem(iface);
+                var clock = new Clock(iface);
+                var wlan = new WLan(iface);
+                sysconfig.Refresh();
+
+                if (!string.IsNullOrEmpty(mac))
+                {
+                    if (mac != sysconfig.MacAddress)
+                    {
+                        Console.WriteLine($"Skipping device (mac mismatch {mac} != {sysconfig.MacAddress}");
+                        return true;
+                    }
+                }
+                Console.WriteLine($"MAC={sysconfig.MacAddress} DID={sysconfig.DeviceId} FID={sysconfig.FirmwareId} Alias={sysconfig.Alias}");
+
+                if (!string.IsNullOrEmpty(alias))
+                {
+                    Console.Write("Setting alias...");
+                    sysconfig.SetAlias(alias);
+                    Console.WriteLine("OK");
+                }
+
+                Console.Write("Setting date/time...");
+                clock.DateTime = DateTime.Now;
+                Console.WriteLine("OK");
+
+                Console.Write("Setting WLAN params...");
+                wlan.AssociateWithStation(ssid, password, enctype);
+                Console.WriteLine("OK");
+                return true;
+            }
+            catch (Exception any)
+            {
+                Console.WriteLine($"error: {any.Message}");
+                return false;
+            }
+
+
+        }
+
+    }
+}
---
+++ Crypton.TPLinkPlug.DeviceEnroll/Program.cs
@@ -0,0 +1,93 @@
+﻿using Mono.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypton.TPLinkPlug.DeviceEnroll
+{
+    class Program
+    {
+
+        static EnrollProfile currentProfile = null;
+
+
+
+        static void Main(string[] args)
+        {
+            currentProfile = new EnrollProfile();
+
+            var options = new OptionSet();
+            options.Add("profile=", "Specifies device enrollment profile", (string profile) =>
+            {
+                if (File.Exists(profile))
+                {
+                    string filecontent = File.ReadAllText(profile);
+                    try
+                    {
+                        currentProfile = JsonConvert.DeserializeObject<EnrollProfile>(filecontent);
+                    }
+                    catch (Exception any)
+                    {
+                        Console.WriteLine($"tpsp-enroll: invalid profile json: {any.Message}");
+                        Environment.Exit(1);
+                    }
+                    Console.WriteLine($"Loaded profile JSON: {profile}");
+                }
+            });
+            options.Add("ip=", "Specifies device IP for control (192.168.0.1 is default)", (string ip) =>
+            {
+                currentProfile.defaults.ip = ip;
+            });
+            options.Add("ssid=", "Specifies connection AP SSID", (string ssid) =>
+            {
+                currentProfile.defaults.ssid = ssid;
+            });
+            options.Add("password=", "Specifies AP password", (string password) =>
+            {
+                currentProfile.defaults.password = password;
+            });
+            options.Add("enc=", "Specifies AP encryption type (Open, WEP, WPA, WPA2)", (string enctype) =>
+            {
+                currentProfile.defaults.encryptionType = (WLanKeyType)Enum.Parse(typeof(WLanKeyType), enctype);
+            });
+            options.Add("alias=", "Specifies device alias", (string alias) =>
+            {
+                currentProfile.defaults.alias = alias;
+            });
+
+            if (args.Length == 0)
+            {
+                ShowUsage(options);
+                return;
+            }
+
+            List<string> extra;
+            try
+            {
+                extra = options.Parse(args);
+            }
+            catch (OptionException ex)
+            {
+                Console.WriteLine($"tpsp-enroll: {ex.Message}");
+                ShowUsage(options);
+                return;
+            }
+
+            currentProfile.EnrollDevices();
+        }
+
+        private static void ShowUsage(OptionSet p)
+        {
+            Console.WriteLine("Usage: tpsp-enroll OPTIONS");
+            Console.WriteLine("Enroll a TP-Link SmartPlug device onto a network of your choice");
+            Console.WriteLine("without pesky phone apps and stormy clouds");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            p.WriteOptionDescriptions(Console.Out);
+        }
+    }
+}
